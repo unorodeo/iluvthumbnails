21b0159bd518c076024c7d18afc5a23fe29ded6c
enoabasi <enoabasidream@gmail.com>
2024-11-25 23:24:46 +0100

    update styling on `input` and `form`

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -83,7 +83,7 @@ const FormItem = React.forwardRef<
 		<FormItemContext.Provider value={{ id }}>
 			<div
 				ref={ref}
-				className={cn("space-y-2", className)}
+				className={cn("grid gap-2", className)}
 				{...props}
 			/>
 		</FormItemContext.Provider>
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,7 +7,7 @@ import { focusInput } from "@/lib/focuses";
 
 const inputVariants = cva(
 	[
-		"flex w-full rounded-md border border-input text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+		"flex w-full rounded-md border border-input text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
 		focusInput,
 	],
 	{
@@ -17,7 +17,7 @@ const inputVariants = cva(
 				secondary: "bg-secondary hover:bg-secondary/80",
 			},
 			sizes: {
-				sm: "h-8 px-3",
+				sm: "h-8 px-3 text-sm",
 				md: "h-9 px-3 py-1",
 				lg: "h-10 px-3 py-2",
 			},
