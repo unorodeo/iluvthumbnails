e6e7f46209a96be2461b7735803b119820ee83c3
enoabasi <enoabasidream@gmail.com>
2024-11-26 17:33:40 +0100

    feat: add `SessionProvider` for sessions in client components

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,14 +1,15 @@
 import React, { ReactNode } from "react";
 
+import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
 
 export const Providers: React.FC<{
 	children: ReactNode;
 }> = ({ children }) => {
 	return (
-		<>
+		<SessionProvider>
 			{children}
 			<Toaster />
-		</>
+		</SessionProvider>
 	);
 };
