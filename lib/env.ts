c8a57ae92e714959b6b02cd66f57496b549ae5ec
enoabasi <enoabasidream@gmail.com>
2024-11-26 12:41:52 +0100

    refac: update `.env` variables

--- lib/env.ts
+++ lib/env.ts
@@ -4,8 +4,8 @@ import { z } from "zod";
 export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().min(1),
-
 		AUTH_SECRET: z.string().min(1),
+		ENCRYPT_ROUNDS: z.string().min(2).max(3),
 	},
 	runtimeEnv: process.env,
 });
