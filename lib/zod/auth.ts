61b5875e9e8c0636e8e97763df4d699c71f0cb74
enoabasi <enoabasidream@gmail.com>
2024-11-26 09:40:38 +0100

    feat: add validation errors on auth fields

--- lib/zod/auth.ts
+++ lib/zod/auth.ts
@@ -1,10 +1,13 @@
 import { object, string } from "zod";
 
-const email = string().min(1).max(225).email();
+const email = string()
+	.min(1, "Email must be min 1 character")
+	.max(225, "Email must be max 225 characters")
+	.email("Invalid email");
 
 const password = string()
-	.min(8)
-	.max(25)
+	.min(8, "Password must be min 8 characters")
+	.max(25, "Password must be max 25 characters")
 	.regex(
 		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{0,}/,
 		{
@@ -20,7 +23,9 @@ const signInSchema = object({
 });
 
 const signUpSchema = object({
-	name: string().min(4).max(20),
+	name: string()
+		.min(4, "Name must be min 4 characters")
+		.max(20, "Name must be min 20 characters"),
 	email: email,
 	password: password,
 	confirm: password,
