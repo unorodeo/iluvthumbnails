7a5e9994600c6d23a28ca2d6eddd2e768fdb93d4
enoabasi <enoabasidream@gmail.com>
2024-11-27 10:39:14 +0100

    feat: user button

--- components/features/user-btn.tsx
+++ components/features/user-btn.tsx
@@ -1,10 +1,9 @@
-"use client";
+"use server"
 
 import {
 	CastleIcon,
 	GemIcon,
 	LogOutIcon,
-	UserCircleIcon,
 	UserIcon,
 } from "lucide-react";
 import {
@@ -14,38 +13,49 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { signOut, useSession } from "next-auth/react";
+import { auth, signOut } from "@/lib/auth";
 
+import Avatar from "boring-avatars";
 import Link from "next/link";
 import React from "react";
-import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/cn";
+import { focusRing } from "@/lib/focuses";
 
 /**
- * A user button that can be rendered as client component.
+ * A user button that can be rendered as server component
  */
-export const UserBtn: React.FC = () => {
-	const { data: session } = useSession();
+export const UserBtn: React.FC = async () => {
+	const session = await auth();
 
 	if (!session) {
 		return <span className="font-medium muted">Not authorized</span>;
 	}
-  
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger
-				className={buttonVariants({
-					variant: "ghost",
-					className: "[&_svg]:size-5",
-				})}
+				className={cn(
+					// base
+					"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+					// color
+					"hover:bg-accent hover:text-accent-foreground",
+					// size
+					"h-9 w-9",
+					// focus
+					focusRing
+				)}
 			>
-				Account
 				{session.user.image ? (
 					<span>img</span>
 				) : (
-					<UserCircleIcon className="text-muted-foreground" />
+					<Avatar
+						name={session.user.name as string}
+						variant="beam"
+						size={24}
+					/>
 				)}
 			</DropdownMenuTrigger>
-			<DropdownMenuContent className="w-56">
+			<DropdownMenuContent className="min-w-48">
 				<DropdownMenuItem asChild>
 					<Link href={"/studio"}>
 						<CastleIcon className="text-muted-foreground" />
@@ -65,10 +75,22 @@ export const UserBtn: React.FC = () => {
 					</Link>
 				</DropdownMenuItem>
 				<DropdownMenuSeparator />
-				<DropdownMenuItem onClick={() => signOut()}>
-					<LogOutIcon className="text-muted-foreground" />
-					<span>Sign out</span>
-				</DropdownMenuItem>
+				<form
+					action={async () => {
+						"use server";
+						await signOut();
+					}}
+				>
+					<DropdownMenuItem asChild>
+						<button
+							type="submit"
+							className="w-full"
+						>
+							<LogOutIcon className="text-muted-foreground" />
+							<span>Sign out</span>
+						</button>
+					</DropdownMenuItem>
+				</form>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
