a06c8d7d20d48c462298a5f024dfcb65a8b1092b
enoabasi <enoabasidream@gmail.com>
2024-11-27 10:35:38 +0100

    style: remove `dark` mode for dynamic theming

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/components/themes";
 import { Toaster } from "@/components/ui/toaster";
 
 export const Providers: React.FC<{
@@ -8,8 +9,15 @@ export const Providers: React.FC<{
 }> = ({ children }) => {
 	return (
 		<SessionProvider>
-			{children}
-			<Toaster />
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+				<Toaster />
+			</ThemeProvider>
 		</SessionProvider>
 	);
 };
